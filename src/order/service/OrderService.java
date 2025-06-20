d8232024975c944f86d29cc06aee7cec927cce43
galin <galin-ka1@hotmail.com>
2025-06-20 12:33:57 +0200

    added OrderService

---
+++ src/order/service/OrderService.java
@@ -0,0 +1,25 @@
+package order.service;
+
+import order.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderService {
+    private final List<Order> orders=new ArrayList<>();
+
+    public void addOrder(Order order){
+        //todo
+    }
+
+    public List<Order> getAllOrders(){
+        return null;//todo
+
+    }
+    public int getSize(){
+        return orders.size();
+    }
+    public Order getOrderByName(String name){
+        return null; // todo
+    }
+}
