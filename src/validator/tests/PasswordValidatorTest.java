2b4f78f7f2b52bd719a0cf200756d9b2b9a3aeef
galin <galin-ka1@hotmail.com>
2025-06-20 10:35:14 +0200

    added tests: testValidPassword, testSymbolsLessThan8, testInvalidSpecSymbol, testNoUpperCase

--- src/validator/tests/PasswordValidatorTest.java
+++ src/validator/tests/PasswordValidatorTest.java
@@ -1,5 +1,38 @@
 package validator.tests;
 
+import org.junit.jupiter.api.Test;
+import validator.utils.PasswordValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordValidatorTest {
+
+    @Test
+    void testValidPassword(){
+        assertTrue(PasswordValidator.isStrong("Qwerty0!"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0?"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0@"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0#"));
+
+    }
+
+
+    @Test
+    void testSymbolsLessThen8(){
+        assertFalse(PasswordValidator.isStrong("Qwerty0!"));
+
+    }
+    @Test
+    void testInvalidSpecSymbol(){
+        assertFalse(PasswordValidator.isStrong("Qwerty0 "));
+        assertFalse(PasswordValidator.isStrong("Qwer^ty0"));
+        assertFalse(PasswordValidator.isStrong("*Qwerty0"));
+    }
+    @Test
+    void testNoUpperCase(){
+        assertFalse(PasswordValidator.isStrong("qwerty2!"));
+    }
+
+
 
 }
