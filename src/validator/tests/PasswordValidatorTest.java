d2ac7ef288c4bf9359c23eec2405c9f4e403da4e
galin <galin-ka1@hotmail.com>
2025-06-20 11:44:42 +0200

    added implements method isStrong with tests.

--- src/validator/tests/PasswordValidatorTest.java
+++ src/validator/tests/PasswordValidatorTest.java
@@ -1,38 +1,54 @@
 package validator.tests;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import validator.utils.PasswordValidator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordValidatorTest {
+    static PasswordValidator validator;
+    @BeforeAll
+   static void setAll(){
+        validator=new PasswordValidator();
+    }
 
     @Test
     void testValidPassword(){
-        assertTrue(PasswordValidator.isStrong("Qwerty0!"));
-        assertTrue(PasswordValidator.isStrong("Qwerty0?"));
-        assertTrue(PasswordValidator.isStrong("Qwerty0@"));
-        assertTrue(PasswordValidator.isStrong("Qwerty0#"));
+        assertTrue(validator.isStrong("Qwerty0!"));
+        assertTrue(validator.isStrong("Qwerty0?"));
+        assertTrue(validator.isStrong("Qwerty0@"));
+        assertTrue(validator.isStrong("Qwerty0#"));
 
     }
 
 
     @Test
     void testSymbolsLessThen8(){
-        assertFalse(PasswordValidator.isStrong("Qwerty0!"));
+        assertFalse(validator.isStrong("Qwerty0!"));
 
     }
     @Test
     void testInvalidSpecSymbol(){
-        assertFalse(PasswordValidator.isStrong("Qwerty0 "));
-        assertFalse(PasswordValidator.isStrong("Qwer^ty0"));
-        assertFalse(PasswordValidator.isStrong("*Qwerty0"));
+        assertFalse(validator.isStrong("Qwerty0 "));
+        assertFalse(validator.isStrong("Qwer^ty0"));
+        assertFalse(validator.isStrong("*Qwerty0"));
     }
     @Test
     void testNoUpperCase(){
-        assertFalse(PasswordValidator.isStrong("qwerty2!"));
+        assertFalse(validator.isStrong("qwerty2!"));
     }
-
-
+    @Test
+    void testNotLOwerCase(){
+        assertFalse(validator.isStrong("QWERTY2!"));
+    }
+@Test
+    void testNoDigit(){
+        assertFalse(validator.isStrong("Qwerty!!"));
+}
+@Test
+    void testNull(){
+        assertFalse(validator.isStrong(null));
+}
 
 }
--- src/validator/utils/PasswordValidator.java
+++ src/validator/utils/PasswordValidator.java
@@ -1,7 +1,22 @@
 package validator.utils;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 public class PasswordValidator {
+     //проверка через Pattern
+    private static final List<Predicate<String>> RULES =List.of(
+             Pattern.compile(".*[A-Z].*").asPredicate(),
+             Pattern.compile(".*[a-z].*").asPredicate(), //
+             Pattern.compile(".*//d.*").asPredicate(),
+             Pattern.compile(".*[!?@#].*").asPredicate()
+                  );
+
     public static boolean isStrong(String password){
-        return false;
+        return password !=null
+                && password.length()>=8
+                && RULES.stream().allMatch(rule->rule.test(password));
+
     }
 }
